cc4151c3713419c0f77d2d240f7a93295254822d
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:47:37 -0500

    Add print statements to test_cache.py for debugging

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -3,7 +3,11 @@ from src.gnomefinance.cache import PriceCache
 
 cache = PriceCache(duration=30)
 
+print("Cache has Bitcoin:", cache.has("bitcoin"))
+
 cache.save("bitcoin", 65000)
+
+print("Cache has Bitcoin:", cache.has("bitcoin"))
 
 price = cache.get("bitcoin")
 
