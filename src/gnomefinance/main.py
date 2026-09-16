fa6ae3d4f3471a124d016f75dc20734c28cb3f04
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:18:29 -0500

    Refactor main.py to use get_price for assets

--- src/gnomefinance/main.py
+++ src/gnomefinance/main.py
@@ -1,15 +1,22 @@
-
 from .assets import Asset
-from .prices import get_bitcoin_price
+from .prices import get_price
 
 
 def main():
-    bitcoin_price = get_bitcoin_price()
-
-    bitcoin = Asset("Bitcoin", "BTC", bitcoin_price)
+    bitcoin = Asset("Bitcoin", "BTC", get_price("bitcoin"))
+    ethereum = Asset("Ethereum", "ETH", get_price("ethereum"))
+    solana = Asset("Solana", "SOL", get_price("solana"))
 
     print("Welcome to GNOMEfinance!")
+    print()
+
     bitcoin.show()
+    print()
+
+    ethereum.show()
+    print()
+
+    solana.show()
 
 
 if __name__ == "__main__":
