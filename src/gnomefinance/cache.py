7998df15068fd58ae96936af7be41dffae81c7ca
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:39:23 -0500

    Implement PriceCache class for caching prices

---
+++ src/gnomefinance/cache.py
@@ -0,0 +1,26 @@
+import time
+
+
+class PriceCache:
+    def __init__(self, duration=30):
+        self.duration = duration
+        self.prices = {}
+
+    def save(self, coin_id, price):
+        self.prices[coin_id] = {
+            "price": price,
+            "time": time.time()
+        }
+
+    def get(self, coin_id):
+        if coin_id not in self.prices:
+            return None
+
+        saved_data = self.prices[coin_id]
+
+        age = time.time() - saved_data["time"]
+
+        if age > self.duration:
+            return None
+
+        return saved_data["price"]
