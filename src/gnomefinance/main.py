cbe5ff22d5f474f80d9d87515235075211eb1258
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:45:05 -0500

    Implement error handling in get_prices function
    Added error handling for price retrieval.

--- src/gnomefinance/main.py
+++ src/gnomefinance/main.py
@@ -3,21 +3,27 @@ from .prices import get_price
 from .price_data import Price
 from .display import show_header, show_price, show_footer
 from .refresh import RefreshController
+from .errors import PriceAPIError
 
 
 def get_prices():
     prices = []
 
     for coin_id, symbol in COINS.items():
-        usd_price = get_price(coin_id)
+        try:
+            usd_price = get_price(coin_id)
 
-        price = Price(
-            coin_id,
-            symbol,
-            usd_price
-        )
+            price = Price(
+                coin_id,
+                symbol,
+                usd_price
+            )
 
-        prices.append(price)
+            prices.append(price)
+
+        except PriceAPIError as error:
+            print(f"Warning: {error}")
+            print()
 
     return prices
 
