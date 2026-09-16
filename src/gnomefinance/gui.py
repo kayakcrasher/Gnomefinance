8ef47b6653ce903859cca45825656d5700502417
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 17:00:01 -0500

    Add labels for Bitcoin, Ethereum, and Solana

--- src/gnomefinance/gui.py
+++ src/gnomefinance/gui.py
@@ -14,4 +14,25 @@ def start_gui():
 
     title.pack(pady=20)
 
+    bitcoin = tk.Label(
+        window,
+        text="BTC: Loading..."
+    )
+
+    bitcoin.pack()
+
+    ethereum = tk.Label(
+        window,
+        text="ETH: Loading..."
+    )
+
+    ethereum.pack()
+
+    solana = tk.Label(
+        window,
+        text="SOL: Loading..."
+    )
+
+    solana.pack()
+
     window.mainloop()
