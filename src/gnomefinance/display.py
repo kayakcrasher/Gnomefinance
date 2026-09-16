7320fbb603560f35066e837881b8616f43efcb99
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:34:18 -0500

    Add display functions for header, price, and footer

---
+++ src/gnomefinance/display.py
@@ -0,0 +1,14 @@
+def show_header():
+    print("=" * 40)
+    print("           GNOMEfinance")
+    print("=" * 40)
+    print()
+
+
+def show_price(price):
+    price.show()
+    print()
+
+
+def show_footer():
+    print("=" * 40)
