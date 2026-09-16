30e89a033d36413fc95b718aeb85b0c70e8be204
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 16:59:10 -0500

    Add title label to the GUI window

--- src/gnomefinance/gui.py
+++ src/gnomefinance/gui.py
@@ -1,7 +1,17 @@
+import tkinter as tk
+
 from .window import create_window
 
 
 def start_gui():
     window = create_window()
+
+    title = tk.Label(
+        window,
+        text="GNOMEfinance",
+        font=("Arial", 20)
+    )
+
+    title.pack(pady=20)
 
     window.mainloop()
