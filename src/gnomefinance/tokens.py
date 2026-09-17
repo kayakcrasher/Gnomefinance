3c5d50d1b4d1bf5744d7d6a096034e8fe484986e
kayakcrasher <unlimiteddata247@gmail.com>
2026-09-16 19:04:48 -0500

    Add cryptocurrency tokens data structure

---
+++ src/gnomefinance/tokens.py
@@ -0,0 +1,27 @@
+TOKENS = {
+    "bitcoin": {
+        "name": "Bitcoin",
+        "symbol": "BTC",
+        "network": "bitcoin",
+    },
+    "ethereum": {
+        "name": "Ethereum",
+        "symbol": "ETH",
+        "network": "ethereum",
+    },
+    "solana": {
+        "name": "Solana",
+        "symbol": "SOL",
+        "network": "solana",
+    },
+    "cardano": {
+        "name": "Cardano",
+        "symbol": "ADA",
+        "network": "cardano",
+    },
+    "avalanche": {
+        "name": "Avalanche",
+        "symbol": "AVAX",
+        "network": "avalanche",
+    },
+}
